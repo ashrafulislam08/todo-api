c790c3da1209e6890cefad9082d74c4a9c8293ae
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-20 23:12:11 +0600

    Added login functionality

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userSchema = require("../schemas/userSchema");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const User = new mongoose.model("User", userSchema);
 const router = express.Router();
 
@@ -22,6 +23,42 @@ router.post("/signup", async (req, res) => {
     .catch((error) => {
       console.log(error);
     });
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  const user = await User.find({ username: req.body.username });
+
+  if (user && user.length > 0) {
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user[0]?.password
+    );
+
+    if (isValidPassword) {
+      // generate jwt token
+      const token = jwt.sign(
+        { username: user[0].username },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "5hr",
+        }
+      );
+
+      res.status(200).json({
+        access_token: token,
+        message: "login successful!",
+      });
+    } else {
+      res.status(401).json({
+        error: "Authentication failed",
+      });
+    }
+  } else {
+    res.status(401).json({
+      error: "Authentication failed",
+    });
+  }
 });
 
 module.exports = router;
